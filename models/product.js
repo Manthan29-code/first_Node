64f4efe9bca59f8137df3dcb60910bbeadad0e95
Manthan29-code <manthanmovaliya9@gmail.com>
2025-10-06 20:56:42 +0530

    create order and product model

--- models/product.js
+++ models/product.js
@@ -8,8 +8,30 @@ const productSchema = new mongoose.Schema({
     name : {
         require : true , 
         type : String
+    },
+    productImage : {
+        type : String 
+    },
+    price : {
+        type : Number , 
+        default : 0 ,
+    },
+    stock : {
+        default : 0 ,
+        type : Number 
+    },
+    category : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Category",
+        required : true 
+    },
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+
     }
-} , { timestamps : true})
+} , { timestamps : true}
+)
 
 const Product = mongoose.model("Product" , productSchema)
 
