fd8cc127e7a32bbeab292ee8c1985304f59d7607
Manthan29-code <manthanmovaliya9@gmail.com>
2025-10-12 09:47:00 +0530

    create utils

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+const cookieParser  = require("cookie-parser")
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-module.export = {app}
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+module.exports = {app}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path : "./.env"})
 const connectDB = require("./db")
 const { app} = require("./app")
+
 connectDB()
 .then(()=>{
     app.listen( process.env.PORT || 8000 , ()=> {
