998112c50ab007e7909cafaf658de075afbd11b0
Manthan29-code <manthanmovaliya9@gmail.com>
2025-10-28 13:50:09 +0530

    create user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { registerUser , loginUser , logoutUser } = require('../controller/user.controller')
+const { registerUser, loginUser, logoutUser, refreshAccessToken, updateAccountDetails, updateUserAvatar, updateCoverImage, getCurrentUser, changeCurrentPassword, getUserChannelProfile, getWatchHistory} = require('../controller/user.controller')
 const upload = require("../middleware/multerMiddleware")
 const { verifyJWT } = require("../middleware/jwt")
 
@@ -18,5 +18,13 @@ const uploadFields = upload.fields([
 router.post('/register' , uploadFields , registerUser )
 router.post('/login' , loginUser )
 router.get('/logout' , verifyJWT ,logoutUser )
+router.post("/refresh-accessToken" , refreshAccessToken)
+router.get("/change-password" , verifyJWT , changeCurrentPassword)
+router.get("/current-user" , verifyJWT , getCurrentUser)
+router.patch("/update-account" , verifyJWT ,updateAccountDetails )
+router.post('/avatar' , verifyJWT ,upload.single("avatar"), updateUserAvatar )
+router.post("/cover-image" , verifyJWT , upload.single("coverImage") , updateCoverImage)
+router.get("c/:username" , verifyJWT , getUserChannelProfile)
+router.get("/history" , verifyJWT, getWatchHistory)
 
 module.exports = router
